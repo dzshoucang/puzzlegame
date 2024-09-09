9a9c9568d8739fef359e15797d1d11f9092c15da
shoucang <dzshoucang@163.com>
2024-09-09 21:37:43 +0800

    添加图片及打乱顺序

--- src/com/dzsc/ui/GameJFrame.java
+++ src/com/dzsc/ui/GameJFrame.java
@@ -1,9 +1,15 @@
 package com.dzsc.ui;
 
 import javax.swing.*;
+import java.util.Random;
 
 public class GameJFrame extends JFrame {
     // GameJFrame 表示游戏主界面
+
+    //创建一个二维数组
+    //目的：用来管理数据
+    //加载图片的时候会根据二维数组中的数据进行加载
+    int[][] data = new int[4][4];
 
     public GameJFrame(){
         //初始化界面
@@ -12,8 +18,59 @@ public class GameJFrame extends JFrame {
         //初始化菜单
         initJMenuBar();
 
+        //初始化数据（打乱）
+        intData();
+
+        //初始化图片（根据打乱之后的结果去加载图片）
+        initImage();
+
         //让界面显示出来
         this.setVisible(true);
+    }
+
+    //初始化数据（打乱顺序）
+    private void intData() {
+        // 把一维数组中数据0-15打乱顺序，按四个一组放进二维数组
+
+        int[] tempArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        Random r = new Random();
+        for (int i = 0; i < tempArr.length; i++) {
+            int index = r.nextInt(tempArr.length);
+            int temp = tempArr[i];
+            tempArr[i] = tempArr[index];
+            tempArr[index] = temp;
+        }
+        //按四个一组放进二维数组
+        for (int i = 0; i < tempArr.length; i++) {
+            data[i / 4][i % 4] = tempArr[i];
+        }
+
+    }
+
+    // 初始化图片
+    //添加图片时按照二维数组添加
+    private void initImage() {
+        // 外循环 -----把内循环重复执行四次。
+        for (int i = 0; i < 4; i++) {
+            //内循环 在一行添加四个图片
+            for (int j = 0; j  < 4; j ++) {
+                //获取当前要加载的图片序号
+                int number = data[i][j];
+                //创建一个图片ImageIcon的对象
+                ImageIcon icon = new ImageIcon("D:\\JavaProject\\projec\\puzzlegame\\image\\animal\\animal3\\"+ number +".jpg");
+                //创建一个JLabel的对象（管理容器）
+                JLabel jLabel = new JLabel(icon);
+                //指定图片位置
+                jLabel.setBounds(105*j,105*i,105,105);
+                //把管理容器添加到界面中
+
+                this.getContentPane().add(jLabel);
+
+            }
+        }
+
+
+
     }
 
     private void initJMenuBar() {
@@ -55,5 +112,8 @@ public class GameJFrame extends JFrame {
         this.setLocationRelativeTo(null);
         //设置默认关闭方式
         this.setDefaultCloseOperation(3);
+
+        //取消默认的居中放置
+        setLayout(null);
     }
 }
