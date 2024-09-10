8d5e19667642ff4cd785d302345f18dab45783d1
shoucang <dzshoucang@163.com>
2024-09-10 15:48:34 +0800

    完善了菜单功能

--- src/com/dzsc/ui/GameJFrame.java
+++ src/com/dzsc/ui/GameJFrame.java
@@ -2,11 +2,13 @@ package com.dzsc.ui;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
 
-public class GameJFrame extends JFrame implements KeyListener {
+public class GameJFrame extends JFrame implements KeyListener , ActionListener {
     // GameJFrame 表示游戏主界面
 
     //创建一个二维数组
@@ -28,6 +30,16 @@ public class GameJFrame extends JFrame implements KeyListener {
         {13,14,15,0}
     };
 
+    //定义变量用来统计步数
+    int step = 0;
+
+    //创建选项下面的条目
+    JMenuItem replayItem = new JMenuItem("重新游戏");
+    JMenuItem reLoginItem = new JMenuItem("重新登录");
+    JMenuItem closeItem = new JMenuItem("关闭游戏");
+
+    JMenuItem accountItem = new JMenuItem("公众号");
+
     public GameJFrame() {
         //初始化界面
         initJFrame();
@@ -36,7 +48,7 @@ public class GameJFrame extends JFrame implements KeyListener {
         initJMenuBar();
 
         //初始化数据（打乱）
-        intData();
+        initData();
 
         //初始化图片（根据打乱之后的结果去加载图片）
         initImage();
@@ -46,7 +58,7 @@ public class GameJFrame extends JFrame implements KeyListener {
     }
 
     //初始化数据（打乱顺序）
-    private void intData() {
+    private void initData() {
         // 把一维数组中数据0-15打乱顺序，按四个一组放进二维数组
 
         int[] tempArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
@@ -62,10 +74,8 @@ public class GameJFrame extends JFrame implements KeyListener {
             if (tempArr[i] == 0) {
                 x = i / 4;
                 y = i % 4;
-            } else {
-                data[i / 4][i % 4] = tempArr[i];
             }
-
+                data[i / 4][i % 4] = tempArr[i];
         }
 
     }
@@ -81,6 +91,10 @@ public class GameJFrame extends JFrame implements KeyListener {
             winJLabel.setBounds(203,283,197,73);
             this.getContentPane().add(winJLabel);
         }
+
+        JLabel stepCount = new JLabel("步数：" + step);
+        stepCount.setBounds(50,30,100,20);
+        this.getContentPane().add(stepCount);
 
         // 外循环 -----把内循环重复执行四次。
         for (int i = 0; i < 4; i++) {
@@ -117,18 +131,14 @@ public class GameJFrame extends JFrame implements KeyListener {
 
     }
 
+    //初始化菜单
     private void initJMenuBar() {
         //创建整个菜单
         JMenuBar jMenuBar = new JMenuBar();
         //创建菜单上的两个选项
         JMenu functionJMenu = new JMenu("功能");
         JMenu aboutJMenu = new JMenu("关于我们");
-        //创建选项下面的条目
-        JMenuItem replayItem = new JMenuItem("重新游戏");
-        JMenuItem reLoginItem = new JMenuItem("重新登录");
-        JMenuItem closeItem = new JMenuItem("关闭游戏");
 
-        JMenuItem accountItem = new JMenuItem("公众号");
 
         //把条目添加进选项中
         functionJMenu.add(replayItem);
@@ -136,6 +146,12 @@ public class GameJFrame extends JFrame implements KeyListener {
         functionJMenu.add(closeItem);
 
         aboutJMenu.add(accountItem);
+
+        //给条目绑定事件
+        replayItem.addActionListener(this);
+        reLoginItem.addActionListener(this);
+        closeItem.addActionListener(this);
+        accountItem.addActionListener(this);
 
         //把选项添加进菜单
         jMenuBar.add(functionJMenu);
@@ -145,6 +161,7 @@ public class GameJFrame extends JFrame implements KeyListener {
         this.setJMenuBar(jMenuBar);
     }
 
+    //初始化界面
     private void initJFrame() {
         //设置界面的宽高
         this.setSize(603, 688);
@@ -213,6 +230,8 @@ public class GameJFrame extends JFrame implements KeyListener {
             data[x][y]=data[x][y+1];
             data[x][y+1]=0;
             y++;
+            //每移动一次，步数加一
+            step++;
             //按照最新的图片加载图片
             initImage();
 
@@ -228,6 +247,8 @@ public class GameJFrame extends JFrame implements KeyListener {
             data[x][y]=data[x+1][y];
             data[x+1][y]=0;
             x++;
+            //每移动一次，步数加一
+            step++;
             //按照最新的图片加载图片
             initImage();
 
@@ -243,6 +264,8 @@ public class GameJFrame extends JFrame implements KeyListener {
             data[x][y]=data[x][y-1];
             data[x][y-1]=0;
             y--;
+            //每移动一次，步数加一
+            step++;
             //按照最新的图片加载图片
             initImage();
 
@@ -258,6 +281,8 @@ public class GameJFrame extends JFrame implements KeyListener {
             data[x][y]=data[x-1][y];
             data[x-1][y]=0;
             x--;
+            //每移动一次，步数加一
+            step++;
             //按照最新的图片加载图片
             initImage();
 
@@ -285,5 +310,53 @@ public class GameJFrame extends JFrame implements KeyListener {
             }
         }
         return true;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //获取当前被点击的方法
+        Object obj = e.getSource();
+        if(obj == replayItem){
+            System.out.println("重新游戏");
+            //打乱数据
+            initData();
+            //计数器清零
+            step = 0;
+            //重新加载图片
+            initImage();
+
+        } else if (obj == reLoginItem) {
+            System.out.println("重新登录");
+            //关闭当前界面
+            this.setVisible(false);
+            //打开登录界面
+            new LoginJFrame();
+        } else if (obj == closeItem) {
+            System.out.println("关闭游戏");
+            //关闭虚拟机
+            System.exit(0);
+        } else if (obj == accountItem) {
+            System.out.println("公众号");
+            //创建弹窗对象
+            JDialog jDialog = new JDialog();
+            //构建一个管理图片的容器对象JLabel
+            JLabel jLabel = new JLabel(new ImageIcon("puzzlegame\\image\\about.png"));
+            //设置位置和宽高
+            jLabel.setBounds(0,0,258,258);
+            //把图片添加进图片
+            jDialog.getContentPane().add(jLabel);
+            //设置弹窗大小
+            jDialog.setSize(344,344);
+            //弹窗置顶
+            jDialog.setAlwaysOnTop(true);
+            //弹窗居中
+            jDialog.setLocationRelativeTo(null);
+
+            //弹窗不关闭无法操作下面的界面
+            jDialog.setModal(true);
+
+            //让弹窗显示出来
+            jDialog.setVisible(true);
+        }
     }
 }
